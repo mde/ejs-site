0cc3a17bc90b0efb24263cdd5322d678b1071de7
vsn4ik <vsn4ik@gmail.com>
2015-05-18 22:01:48 +0300

    Ignore DS_Store, refactor js, fix html typo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,28 @@
 jQuery(document).ready(function($) {
 
     /* ======= Scrollspy ======= */
-    $('body').scrollspy({ target: '#header', offset: 400});
-    
+    $('body').scrollspy({ target: '#header', offset: 400 });
+
     /* ======= Fixed header when scrolled ======= */
-    
-    $(window).bind('scroll', function() {
-         if ($(window).scrollTop() > 50) {
-             $('#header').addClass('navbar-fixed-top');
-         }
-         else {
-             $('#header').removeClass('navbar-fixed-top');
-         }
+
+    $(window).on('scroll', function() {
+         $('#header').toggleClass('navbar-fixed-top', $(window).scrollTop() > 50);
     });
-   
+
     /* ======= ScrollTo ======= */
-    $('a.scrollto').on('click', function(e){
-        
+    $('a.scrollto').on('click', function(e) {
+
         //store hash
         var target = this.hash;
-                
+
         e.preventDefault();
-        
-		$('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
+
+        $('body').scrollTo(target, 800, { offset: -70, axis: 'y', easing: 'easeOutQuad' });
+
         //Collapse mobile menu after clicking
-		if ($('.navbar-collapse').hasClass('in')){
-			$('.navbar-collapse').removeClass('in').addClass('collapse');
-		}
-		
-	});
+        if ($('.navbar-collapse').hasClass('in')) {
+            $('.navbar-collapse').removeClass('in').addClass('collapse');
+        }
+    });
 
 });
