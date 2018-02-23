77a8996de3d31649813490e69b921b949afcd34c
Kevin Simper <kevin.simper@gmail.com>
2018-02-23 11:32:32 +0100

    Remove scrollTo function and default to browser scroll to
    Closes #10

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,20 +9,4 @@ jQuery(document).ready(function($) {
          $('#header').toggleClass('navbar-fixed-top', $(window).scrollTop() > 50);
     });
 
-    /* ======= ScrollTo ======= */
-    $('a.scrollto').on('click', function(e) {
-
-        //store hash
-        var target = this.hash;
-
-        e.preventDefault();
-
-        $('body').scrollTo(target, 800, { offset: -70, axis: 'y', easing: 'easeOutQuad' });
-
-        //Collapse mobile menu after clicking
-        if ($('.navbar-collapse').hasClass('in')) {
-            $('.navbar-collapse').removeClass('in').addClass('collapse');
-        }
-    });
-
 });
